43241dac5e685aa9d4f8a89467a658e7078a13c5
Lance Hebert <LSUHEBERT@gmail.com>
2022-02-01 21:28:54 -0800

    commit tree 54b329ec426f1f57c6ac337d8864dd5f6760a19f

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,5 +1,7 @@
 class AuthorsController < ApplicationController
-  
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
   def show
     author = Author.find(params[:id])
 
@@ -7,13 +9,19 @@ class AuthorsController < ApplicationController
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
+
   end
 
   private
   
+  def render_unprocessable_entity_response(exception)
+    render json: {errors:exception.record.errors}, status: :unprocessable_entity
+  end
+
+  
+
   def author_params
     params.permit(:email, :name)
   end
